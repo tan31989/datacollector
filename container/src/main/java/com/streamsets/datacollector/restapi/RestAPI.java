f1390ffdef966af729c222bef3d189455175e029
Madhukar <madhu@streamsets.com>
2021-04-28 10:08:48 -0700

    SDC-16922. WebSocketToRestDispatcher - for ping request - MessageBodyReader not found for media type=text/html
    Change-Id: I6352837da89001fcc89f43e6456b83799be1f04a
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/41079
    Reviewed-by: Virag Kothari <virag@streamsets.com>

--- container/src/main/java/com/streamsets/datacollector/restapi/RestAPI.java
+++ container/src/main/java/com/streamsets/datacollector/restapi/RestAPI.java
@@ -18,12 +18,15 @@ package com.streamsets.datacollector.restapi;
 import javax.annotation.security.PermitAll;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/ping")
 @PermitAll
 public class RestAPI {
 
   @GET
+  @Produces(MediaType.TEXT_PLAIN)
   public String ping() {
     return "pong" + System.lineSeparator();
   }
